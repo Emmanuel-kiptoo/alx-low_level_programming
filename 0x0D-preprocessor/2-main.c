eb38dd7e4945c8b2f323191efd4f8f993512d005
emmanuel.kiptoo <kiptooemmanuel954@gmail.com>
2023-03-20 08:44:50 -0700

    0x0D-preprocessor

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
 
 /**
- * main - prints the name of the file
- *
+ * main - Entry point
  * Return: Always 0 (Success)
  */
-int ain(void)
+int main(void)
 {
 	printf("%s\n", __FILE__);
 	return (0);
 }
-
-
-
